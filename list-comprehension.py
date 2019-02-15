76680c0df3ebccdc18b0acc3efb4053438da672d
njdevengine <newjerseydevelopment@gmail.com>
2019-02-14 19:11:59 -0500

    Create list-comprehension.py

---
+++ list-comprehension.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+tick_locations = []
+for value in x_axis:
+    tick_locations.append(value)
+
+x_axis - np.arange(len(rain_df))
+tick_locations = [value for value in x_axis]
