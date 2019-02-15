082aba88c7e8991dc4d29733042f990d75533154
njdevengine <newjerseydevelopment@gmail.com>
2019-02-14 19:17:41 -0500

    Update list-comprehension.py

--- list-comprehension.py
+++ list-comprehension.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
 
+#these two syntaxes are the same
+
+x_axis = np.arange(len(dataframe))
+
 tick_locations = []
 for value in x_axis:
     tick_locations.append(value)
 
-x_axis - np.arange(len(rain_df))
 tick_locations = [value for value in x_axis]
